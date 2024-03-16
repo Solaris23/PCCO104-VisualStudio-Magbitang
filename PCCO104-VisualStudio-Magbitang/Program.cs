3732adcb2b43a6c1fd912f6270c6df2478192818
User <User@DESKTOP-AMHGCN6>
2024-03-16 11:12:56 +0800

    Add project files.

---
+++ PCCO104-VisualStudio-Magbitang/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace PCCO104_VisualStudio_Magbitang
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            
+            String[] names = { "name1", "name2" };
+
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
